adb84da568e7471ef9f8154e88b3a95a153a6438
TannerMantooth <137127335+TannerMantooth@users.noreply.github.com>
2023-06-27 19:39:57 -0500

    Added comments for code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
+Creation of the 'showPage' function
+Creates pages that contain 9 student items
 */
 function showPage(list, page) {
    const startIndex = (page * 9) - 9;
@@ -8,9 +8,10 @@ function showPage(list, page) {
    const studentList = document.querySelector('ul.student-list');
    studentList.innerHTML = '';
 
+   //creates each list item for the length of the student array then adds it to the student-list unordered list
    for (let i = 0; i <list.length; i++) {
       if (i >= startIndex && i < endIndex) {
-         const studentItem = studentList.insertAdjacentHTML('beforeend', `
+         const student = `
          <li class="student-item cf">
             <div class="student-details">
                <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture"
@@ -20,29 +21,35 @@ function showPage(list, page) {
             <div class="joined-details">
                <span class="date">${list[i].registered.date}</span>
             </div>
-         </li>`)
+         </li>`;
+         studentList.insertAdjacentHTML('beforeend', student);
       }
    }
 }
 
 /*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
+Creation of the 'addPagination' function
+Creates the pagination buttons at the bottom of the page for navigation
 */
 function addPagination(list) {
    const paginationButtons = Math.ceil(list.length / 9);
    const linkList = document.querySelector('ul.link-list');
    linkList.innerHTML = '';
 
+   //loops through to create buttons based off of value stored in paginationButtons
    for (let i = 1; i <= paginationButtons; i++) {
       linkList.insertAdjacentHTML('beforeend', `
       <li>
          <button type="button">${i}</button>
       </li>`)
    }
-   document.querySelector('button').className = "active";
 
+   //assigning active class to first button
+   document.querySelector('li button').className = "active";
+
+   //listener for navigation buttons at bottom of page
    linkList.addEventListener('click', (e) => {
+      const button = e.target;
       if (e.target.tagName === "BUTTON") {
          document.querySelector('.active').className = '';
          e.target.className = "active";
